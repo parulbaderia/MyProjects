f41f41821b7b6f439159fdf09e6cac00c6698c13
parulbaderia <137832452+parulbaderia@users.noreply.github.com>
2023-06-26 17:07:27 -0400

    Add files via upload

---
+++ Insertion_queries.sql
@@ -0,0 +1,114 @@
+-- insert some values
+
+INSERT INTO SPEED
+VALUES(1,'super saver shipping');
+INSERT INTO SPEED
+VALUES(2,'standard shipping');
+INSERT INTO SPEED
+VALUES(3,'two‐day shipping');
+INSERT INTO SPEED
+VALUES(4,'one‐day shipping');
+
+-- insert some values
+INSERT INTO CUSTOMERS
+VALUES(1, 'Tom', 'Smith','84,Florence St,Worcester,MA,01603','smith@gmail.com',7746961234);
+INSERT INTO CUSTOMERS
+VALUES(2,'Ashi','Baderia','702,Woodland St,South Windsor,CT,06028','ashi@xyz.com',7746965678);
+INSERT INTO CUSTOMERS
+VALUES(3,'Shvetank','Baderia','46,Townhill St,Hartford,CT,06105','shvetank@xyz.com',8571234567);
+INSERT INTO CUSTOMERS
+VALUES(4,'Nishita','Baderia','201,Main St,Worcester,MA,06010','nishi@xyz.com',3216547890);
+INSERT INTO CUSTOMERS
+VALUES(5,'Ankit','Jain','901,Clark St,South Windsor,CT,06028','ankit@abc.com',987654321);
+
+-- insert some values
+
+INSERT INTO PROD_CATEGORY 
+VALUES(1,'Electronics');
+INSERT INTO PROD_CATEGORY
+VALUES(2,'Computers');
+INSERT INTO PROD_CATEGORY
+VALUES(3,'Appliances');
+INSERT INTO PROD_CATEGORY
+VALUES(4,'Beauty');
+INSERT INTO PROD_CATEGORY
+VALUES(5,'Pharmacy');
+INSERT INTO PROD_CATEGORY
+VALUES(6,'Clothing');
+
+-- insert some values
+
+INSERT INTO SELLER
+VALUES(1,'A2Z Electronics','New York City',1212121212,'a2z@gmail.com');
+INSERT INTO SELLER
+VALUES(2,'Future Computers','Jersey city',3456456345,'future@xyz.com');
+INSERT INTO SELLER
+VALUES(3,'Smart kitchen store','Atlanta',88888776767,'kitchen@abc.com');
+INSERT INTO SELLER
+VALUES(4,'Glamour','California',4545667809,'glam@xyz.com');
+INSERT INTO SELLER
+VALUES(5,'CVS Pharmacy','Worcester',7746960909,'cvs@gmail.com');
+INSERT INTO SELLER
+VALUES(6,'Comfort wear','Boston',9898989567,'wear@abc.com');
+
+-- insert some values
+
+INSERT INTO PRODUCTS
+VALUES(1,1,1,'Smart TV','32 inches LED TV',300);
+INSERT INTO PRODUCTS
+VALUES(2,1,1,'Fire  TV stick','Fire TV stick with Alexa',26.99);
+INSERT INTO PRODUCTS
+VALUES(3,2,2,'Acer Aspire','Laptop 11th gen',269);
+INSERT INTO PRODUCTS
+VALUES(4,2,2,'HP Printer','HP high quality Colored laser printer',149);
+INSERT INTO PRODUCTS
+VALUES(5,3,3,'Coffee grinder','stainless steel coffee grinder',22 );
+INSERT INTO PRODUCTS
+VALUES(6,4,4,'Rosy red lipstick','Matt finish rosy red lipstick',29.99);
+INSERT INTO PRODUCTS
+VALUES(7,4,4,'Nail paint','Turquoise nail paint with no harmful chemicals',12);
+INSERT INTO PRODUCTS
+VALUES(8,5,5,'Cough syrup','Honey-ginger night cough syrup',15 );
+INSERT INTO PRODUCTS
+VALUES(9,6,6,'Fleece leggings','Comfortable fleece leggings for winters',22);
+
+-- insert some values
+
+INSERT INTO INVENTORY 
+VALUES(1,1,1,1,5,'New');
+INSERT INTO INVENTORY
+VALUES(2,1,2,1,8,'New');
+INSERT INTO INVENTORY
+VALUES(3,2,3,2,15,'New');
+INSERT INTO INVENTORY
+VALUES(4,2,4,2,16,'Refurbished');
+INSERT INTO INVENTORY
+VALUES(5,3,5,3,3,'used');
+INSERT INTO INVENTORY
+VALUES(6,4,6,4,5,'New');
+INSERT INTO INVENTORY
+VALUES(7,4,7,4,10,'New');
+INSERT INTO INVENTORY 
+VALUES(8,5,8,5,4,'New');
+INSERT INTO INVENTORY
+VALUES(9,6,9,6,7,'New');
+
+-- insert some values
+
+INSERT INTO ORDERS
+VALUES(2,2,1,1);
+
+-- insert some values
+INSERT INTO PACKAGE
+VALUES(4,4,3);
+
+-- insert some values
+INSERT INTO NOTIFICATIONS
+VALUES(1,6,1);
+
+-- insert some values
+INSERT INTO NOTIFICATIONS
+VALUES(2,7,2);
+
+
+
